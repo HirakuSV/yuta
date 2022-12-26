1badb9c7f5b0e757e5cc2e270e4cf3e20c1aa89a
HirakuSV <pbawane899@gmail.com>
2022-12-26 16:02:25 +0530

    Fixed some too

--- source/commands/pause.js
+++ source/commands/pause.js
@@ -5,6 +5,7 @@ module.exports = {
         .setName('pause')
         .setDescription('pause playing the music.'),
     async execute(interaction, yuta) {
+        if (!interaction.member.voice.channel) return interaction.reply('You need to be in a voice channel.');
         const player = yuta.player.get(interaction.guild.id);
         await player.pause();
         interaction.reply('Paused playing.');
