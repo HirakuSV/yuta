954cb4bb92c539608acd35e5fedabeccf3d85875
HirakuSV <pbawane899@gmail.com>
2023-02-11 11:04:08 +0530

    Added player changes.

--- source/commands/play.js
+++ source/commands/play.js
@@ -3,7 +3,7 @@ const { joinVoiceChannel, createAudioPlayer, createAudioResource, NoSubscriberBe
 const ytdl = require('ytdl-core');
 const ytsearch = require('yt-search');
 const mcEmbed = require('../utils/mcEmb');
-const imPlayer = require('../utils/player');
+const streamPlayer = require('../utils/player');
 const { PermissionsBitField } = require('discord.js');
 global.AbortController = require('node-abort-controller').AbortController;
 
@@ -15,7 +15,7 @@ module.exports = {
     .setRequired(true)),
   async execute(interaction, yuta) {
     if (!interaction.member.voice.channel) return interaction.reply('You need to be in a voice channel.');
-    if (!interaction.member.voice.channel.permissionsFor(interaction.guild.me).has([PermissionsBitField.Flags.Connect, PermissionsBitField.Flags.Speak])) return interaction.reply('I don\'t have peermissions to connect/speak to your channel.');
+    if (!interaction.member.voice.channel.permissionsFor(interaction.guild.members.me).has([PermissionsBitField.Flags.Connect, PermissionsBitField.Flags.Speak])) return interaction.reply('I don\'t have peermissions to connect/speak to your channel.');
     await interaction.deferReply();
     const guildQueue = yuta.queue.get(interaction.guild.id);
     const query = interaction.options.getString('song');
@@ -42,6 +42,7 @@ module.exports = {
       }
       queueConstructor.songs.push(song);
       yuta.queue.set(interaction.guild.id, queueConstructor);
+
       try {
         const connection = await joinVoiceChannel({
           channelId: interaction.member.voice.channel.id,
@@ -49,7 +50,7 @@ module.exports = {
           adapterCreator: interaction.guild.voiceAdapterCreator
         });
         queueConstructor.connection = connection;
-        streamPlayer(interaction.guild.id, queueConstructor.songs[0]);
+        streamPlayer(interaction.guild.id, queueConstructor.songs[0], yuta);
       } catch (error) {
         yuta.queue.delete(interaction.guild.id);
         interaction.channel.send(`Error connecting to the channel, \`${error}\``);
@@ -58,50 +59,15 @@ module.exports = {
     } else {
       guildQueue.songs.push(song);
     }
-    yuta.skip.set(interaction.guild.id, skip);
-    var songQueue = yuta.queue.get(interaction.guild.id);
-    async function streamPlayer(guildId, songStream) {
-      if (!songStream) {
-        songQueue.connection.destroy();
-        yuta.queue.delete(guildId);
-        songQueue.textChannel.send('No more songs to play');
-        return;
-      }
 
-      const streamCache = await ytdl(songStream.url, { filter: 'audioonly' });
-      const stream = await createAudioResource(streamCache);
-      const player = createAudioPlayer({ behaviors: { noSubscriber: NoSubscriberBehavior.Pause } });
-      player.play(stream);
-      songQueue.connection.subscribe(player);
-
-      player.on(AudioPlayerStatus.Idle, () => {
-        if (!songQueue) {
-          songQueue.connection.destroy();
-          return songQueue.textChannel.send('No more songs play, leaving the vc.');
-        } else {
-          songQueue.songs.shift();
-          streamPlayer(guildId, songQueue.songs[0]);
-        }
-      });
-      songQueue.textChannel.send({ embeds: [mcEmbed(songStream.thumbnail, songStream.title, `Now playing **${songStream.title}**\nRequested by ${songStream.author}`, yuta.user.displayAvatarURL())] });
-      yuta.player.set(interaction.guild.id, player);
-      player.on('error', (err) => {
-        songQueue.textChannel.send(`**${songQueue.songs[0].title}** has encoding errors.`);
-        if (songQueue.songs.length < 1) {
-          skip();
-        } else {
-          songQueue.connection.destroy();
-          yuta.queue.delete(interaction.guild.id);
-        }
-      });
-    }
     async function skip(ins) {
       if (!songQueue.songs.length) return songQueue.textChannel.send('No songs to skip to.')
       songQueue.songs.shift();
-      streamPlayer(interaction.guild.id, songQueue.songs[0]);
+      streamPlayer(interaction.guild.id, songQueue.songs[0], yuta);
       if (ins) {
         ins.reply({ embeds: [mcEmbed(songQueue.songs[0].thumbnail, songQueue.songs[0].title, `Skipped to **${songQueue.songs[0].title}**\nRequested by ${songQueue.songs[0].author}`, yuta.user.displayAvatarURL())] })
       } else songQueue.textChannel.send({ embeds: [mcEmbed(songQueue.songs[0].thumbnail, songQueue.songs[0].title, `Skipped to **${songQueue.songs[0].title}**\nRequested by ${songQueue.songs[0].author}`, yuta.user.displayAvatarURL())] })
     }
+    yuta.skip.set(interaction.guild.id, skip);
   },
 };
